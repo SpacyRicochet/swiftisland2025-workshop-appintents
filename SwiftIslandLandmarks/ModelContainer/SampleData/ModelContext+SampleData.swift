824a64d548d648e8ac74171723cd9a789018961a
Bruno Scheele <bruno@noodlewerk.com>
2025-08-07 21:39:10 +0200

    add: reset sample data

--- SwiftIslandLandmarks/ModelContainer/SampleData/ModelContext+SampleData.swift
+++ SwiftIslandLandmarks/ModelContainer/SampleData/ModelContext+SampleData.swift
@@ -1,3 +1,4 @@
+import Foundation
 import SwiftData
 
 extension ModelContext {
@@ -9,5 +10,30 @@ extension ModelContext {
 		for collection in LandmarkCollection.initialList {
 			insert(collection)
 		}
+	}
+	
+	func eraseAllData() {
+		do {
+			for model in try fetch(FetchDescriptor<LandmarkCollection>()) {
+				delete(model)
+			}
+			for model in try fetch(FetchDescriptor<Landmark>()) {
+				delete(model)
+			}
+			for model in try fetch(FetchDescriptor<Visit>()) {
+				delete(model)
+			}
+			for model in try fetch(FetchDescriptor<AppContext>()) {
+				delete(model)
+			}
+		} catch {
+			fatalError("Could not delete data: \(error.localizedDescription)")
+		}
+	}
+	
+	func resetSampleData() {
+		eraseAllData()
+		insertSampleData()
+		insert(AppContext(firstLaunchTimestamp: Date()))
 	}
 }
--- SwiftIslandLandmarks/Views/Lists/MainOverview.swift
+++ SwiftIslandLandmarks/Views/Lists/MainOverview.swift
@@ -7,6 +7,7 @@ struct MainOverview: View {
 	@Query private var collections: [LandmarkCollection]
 	
 	@State private var isEditorPresented = false
+	@State private var isResetSampleDataPresented = false
 	@Binding var navigationContext: NavigationContext
 	
 	var body: some View {
@@ -31,12 +32,24 @@ struct MainOverview: View {
 		}
 		.listStyle(.insetGrouped)
 		.toolbar {
+			ToolbarItem(placement: .topBarLeading) {
+				Button("", systemImage: "arrow.clockwise") {
+					isResetSampleDataPresented.toggle()
+				}
+			}
 			ToolbarItem(placement: .topBarTrailing) {
 				EditButton()
 			}
 		}
 		.sheet(isPresented: $isEditorPresented) {
 			EditCollectionForm(collection: nil, deleteHandler: nil)
+		}
+		.alert("Reset sample data?", isPresented: $isResetSampleDataPresented) {
+			Button("Yes, reset sample data", role: .destructive) {
+				modelContext.resetSampleData()
+			}
+		} message: {
+			Text("You can start with a clean slate by deleting everything and restoring the original sample data.")
 		}
 	}
 }
