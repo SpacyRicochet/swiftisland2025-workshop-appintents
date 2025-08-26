07ce748038ae4a7525e58da3477d391eaa05ca12
Bruno Scheele <bruno@noodlewerk.com>
2025-08-26 22:33:11 +0200

    change: add ready-made view

--- 1-create-snippet-view/SwiftIslandLandmarks/AppIntents/Views/LandmarkSnippetView.swift
+++ 1-create-snippet-view/SwiftIslandLandmarks/AppIntents/Views/LandmarkSnippetView.swift
@@ -2,11 +2,28 @@ import SwiftUI
 import AppIntents
 
 struct LandmarkSnippetView: View {
-	// Make sure the landmark can be passed to future intents!
+	// We need to add the landmark here somehow
+	// and make sure it can be passed in future intents.
 	
 	var body: some View {
-		// We want to show the landmark's name
-		// and favorite status.
-		EmptyView()
+		ZStack {
+			LinearGradient(colors: [.yellow, .red], startPoint: .top, endPoint: .bottomTrailing)
+				.opacity(0.3)
+				.clipShape(.containerRelative)
+			HStack {
+				Text(DeleteAfter.name)
+					.font(.headline)
+					.fontDesign(.rounded)
+					.frame(maxWidth: .infinity, alignment: .leading)
+				Image(systemName: DeleteAfter.isFavorite ? "star.fill" : "star")
+					.tint(.red)
+			}
+			.padding()
+		}
 	}
+}
+
+private struct DeleteAfter {
+	static let name = "Prins Hendrik"
+	static let isFavorite = false
 }
